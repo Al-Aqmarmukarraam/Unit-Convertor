f107041c7fd963bf3aa3b639fe38d1047dda2a08
Al-Aqmarmukarraam <159696388+Al-Aqmarmukarraam@users.noreply.github.com>
2025-05-31 18:10:08 +0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,46 @@
+import streamlit as st
+
+st.title("🌍Unit Convertor App")
+st.markdown("### Converts Lenght, Weight and Time Instantly")
+st.write("Welcome! select a category, enter a value and get the converted result in real-time")
+
+category = st.selectbox("Choose a category", ["Lenght", "Weight", "Time"])
+
+def convert_units(category, value, unit):
+    if category == "Lenght":
+        if unit == "Kilometers to Miles":
+            return value * 0.621371
+        elif  unit == "Miles to Kilometers":
+            return value / 0.621371
+    elif category == "Weight":
+        if unit == "Kilograms to Pounds":
+            return value * 2.20462
+        elif unit == "Pounds to Kilograms":
+            return value / 2.20462
+    elif category == "Time":
+        if unit == "Seconds to Minutes":
+            return value / 60
+        elif unit == "Minutes to Seconds":
+            return value * 60
+        elif unit == "Minutes to Hours":
+            return value / 60
+        elif unit == "Hours to Minutes":
+            return value * 60
+        elif unit == "Hours to Days":
+            return value / 24
+        elif unit == "Days to Hours":
+            return value * 24
+        return 0
+if category == "Lenght":
+    unit = st.selectbox("📏 Select Conversion", ["Kilometer to Miles", "Miles to Kilometer"])       
+elif category == "Weight":
+    unit = st.selectbox ("⚖️ Select Conversion", ["Kilograms to Pounds", "Pounds to Kilograms"])
+elif category == "Time":
+    unit = st.selectbox ("⏰ Select Conversion", ["Seconds to Minutes", "Minutes to Seconds", "Minutes to Hours", "Hours to Minutes", "Hours to Days", "Days to Hours"])
+
+# Input and Convert Button shown for all categories
+value = st.number_input("Enter The Value To Convert")
+
+if st.button("Convert"):
+        result = convert_units(category, value, unit)
+        st.success(f"The Result Is {result:.2f}")      
